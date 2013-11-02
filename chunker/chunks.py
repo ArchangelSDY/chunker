2b32c3c68ccaab2616bb1c71e05efac63e2a7f39
Archangel_SDY <Archangel.SDY@gmail.com>
2013-11-03 01:30:17 +0800

    Add docs to chunks.

--- chunker/chunks.py
+++ chunker/chunks.py
@@ -5,6 +5,46 @@ import os
 
 
 class Chunk(object):
+    """
+    Chunk in the file. A chunk usually contains a signature indicating its
+    type, and a group of data fields.
+
+    :param fp: File object to read from.
+    :param parser: The parser it belongs to. It's optional and you can leave it None.
+
+    To define your chunk class, you should follow these steps:
+
+    1. Define its fields. See fields for more information.
+
+    2. Define a static `matches` method to judge if the following bytes match this type of chunk.
+
+    Example::
+
+        import os
+        import struct
+
+        class DataChunk:
+            Fields = (
+                UnsignedLongField('type'),
+                UnsignedLongField('data_length'),
+                StringField('data', length_field_name='data_length'),
+            )
+
+            @staticmethod
+            def matches(fp):
+                buf = fp.read(4)
+                type = struct.unpack('>H', buf)[0]
+
+                # IMPORTANT! Reset position if you need to populate this type
+                # field or type doesn't match
+                fp.seek(-4, os.SEEK_CUR)
+
+                return type == 0x01020304
+
+        if DataChunk.matches(fp):
+            c = DataChunk(fp, None)
+            print(c.data) # Access field value by its name
+    """
     Fields = ()
 
     def __new__(cls, *args, **kargs):
@@ -22,9 +62,22 @@ class Chunk(object):
 
     @staticmethod
     def matches(fp):
+        """
+        Read next a few bytes to judge if the following data match this type
+        of chunk.
+
+        You should override this method or it will return False by default.
+
+        :param fp: File object to read from.
+        :returns: If the following bytes match this chunk type.
+        """
         return False
 
     def populate(self):
+        """
+        Populate chunk fields. After that, you can access each field data
+        directly by field name.
+        """
         for field in self.fields:
             field.populate(self.fp, self)
             if self.parser is not None and self.parser.is_debug:
@@ -39,8 +92,3 @@ class Chunk(object):
             dumps.append('%s: %s' % (field.name, field.value))
         dumps.append('----------')
         return '\n'.join(dumps)
-
-
-class ToTheEndChunk(Chunk):
-    def populate(self, fp):
-        fp.seek(0, os.SEEK_END)
