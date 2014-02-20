ef32f70ca5405110f58f3663bd01a32130aaba59
Archangel_SDY <Archangel.SDY@gmail.com>
2014-02-20 20:45:23 +0800

    Fix docs building.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 # serve to show the default.
 
 import sys, os
-sys.path.insert(0, '../chunker/')
+sys.path.insert(0, '..')
+sys.path.insert(0, '../chunker')
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
